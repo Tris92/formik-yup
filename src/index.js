82a6e44a1be8dd61009d09803b7b4071b39f87e9
Eddo <eddo@macbookpro-tristan.local>
2020-10-23 15:20:22 +0200

    withFormik method

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import { Formik, Field, ErrorMessage } from 'formik';
+import { Field, ErrorMessage, FieldArray, withFormik } from 'formik';
 import * as Yup from 'yup';
 
 
 const CustomInput = ({ field, form, ...props }) => {
   return (
     <div className="form-group">
-      <label> {field.name} </label>
+      <label> {props.displayname ? props.displayname : field.name} </label>
       <input {...field} {...props} type="text" className="form-control" />
     </div>
   )
@@ -23,65 +23,86 @@ const CustomError = (props) => {
 }
 
 
+
+
 class App extends Component {
 
 
-  userSchema = Yup.object().shape({
-    name: Yup.string().min(3, 'Trop court').max(10, 'Trop long').required('required'),
-    email: Yup.string().email('mauvais email').required('required email'),
-    password: Yup.string().min(5, 'Trop court')
-  })
-
-  submit = (values, actions) => {
-    console.log(values);
-    // console.log(actions);
-    actions.setSubmitting(false);
-  }
-
   render() {
+    const {values, isSubmitting, handleSubmit} = {...this.props}
    return (
     <div className="container-fluid p-5 bg-light d-flex flex-column justify-content-center align-items-center">
-      
-      <Formik
-        onSubmit={this.submit}
-        initialValues={ { name:'', email:'', password:'' } }
-        validationSchema={this.userSchema}
-        // validateOnBlur={true} //passer à false pour changer la validation dynamique des champs input
-        // validateOnChange={true} //passer à false pour changer la validation dynamique des champs input
-      >
-        { ({
-          handleChange, 
-          handleBlur, 
-          handleSubmit, 
-          values, 
-          isSubmitting, 
-          errors,
-          touched
-        }) => (
-          <form onSubmit={handleSubmit} className="bg-white p-5 d-flex flex-column">
-            
-            <Field name='name' component={ CustomInput } />
-            <ErrorMessage name="name" component={ CustomError } />
+      <form onSubmit={handleSubmit} className="bg-white p-5 d-flex flex-column">
+        
+        <Field name='name' component={ CustomInput } />
+        <ErrorMessage name="name" component={ CustomError } />
 
-            <Field name='email' type="email" component={ CustomInput } />
-            <ErrorMessage name="email" component={ CustomError } />
-            
-            <Field name='password' type="password" component={ CustomInput } />
-            <ErrorMessage name="password" component={ CustomError } />
+        <Field name='email' type="email" component={ CustomInput } />
+        <ErrorMessage name="email" component={ CustomError } />
+        
+        <Field name='password' type="password" component={ CustomInput } />
+        <ErrorMessage name="password" component={ CustomError } />
 
-            <button type="submit" className="btn btn-small btn-primary" disables={isSubmitting}>Envoyer</button>
-          </form>
-        )}
-      </Formik>
+        <FieldArray name="items" >
+          { arrayHelpers => (
+            <div>
+              { values.items && values.items.length ? (
+                values.items.map((item, index) => (
+                  <div key={index}>
+                    <div> Item: { index } </div> 
+                    <hr className="w-100" /> 
+
+                    <Field name={`items.${index}.name`} displayname="name"  component={ CustomInput } />
+                    <ErrorMessage name={`items.${index}.name`} component={ CustomError } />
+
+                    <Field name={`items.${index}.quantity`} displayname="quantity" component={ CustomInput } />
+                    <ErrorMessage name={`items.${index}.quantity`} component={ CustomError } />
+                    <button type="button" className="btn btn-small btn-danger" onClick={ () => arrayHelpers.remove(index)} >
+                        Delete
+                    </button>
+                  </div>
+                ))
+              ) : null }
+              <button type="button"  className="w-100 btn btn-small btn-success" onClick={() => arrayHelpers.push({
+                name: '',
+                quantity: 0
+                }
+              )} >
+                  Add item
+              </button>
+            </div>
+          ) }
+        </FieldArray>
+
+        <button type="submit" className="my-3 btn btn-small btn-primary" disables={isSubmitting}>Envoyer</button>
+      </form>
     </div>
    )
   }
  }
+
+ const MyForm = withFormik({
+  mapPropsToValues: () => ({ name:'', email:'', password:'', items: [] }),
+  validationSchema: Yup.object().shape({
+    name: Yup.string().min(3, 'Trop court').max(10, 'Trop long').required('required'),
+    email: Yup.string().email('mauvais email').required('required email'),
+    password: Yup.string().min(5, 'Trop court'), 
+    items: Yup.array().of(Yup.object().shape({
+      name: Yup.string().required('required name'),
+      quantity: Yup.number().typeError('Doit être un nombre').min(5, 'Trop faible')
+    }))
+  }),
+  handleSubmit: (values, actions) => {
+    console.log(values);
+    // console.log(actions);
+    actions.setSubmitting(false);
+  }
+})(App)
  
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <MyForm />
   </React.StrictMode>,
   document.getElementById('root')
 );
